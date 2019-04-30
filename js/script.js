b5ce084853a69e022c04cf9cf9269d31a976bbdc
malinaNata <kalinka007@tut.by>
2019-04-30 16:04:39 +0300

    add countdown function

--- js/script.js
+++ js/script.js
@@ -33,4 +33,55 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+
+    //Timer
+    let deadline = '2019-05-03';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60)%60),
+            hours = Math.floor((t/(1000*60*60)));
+            // hours = Math.floor((t/1000/60/60) % 24),
+            // days = Math.floor((t/(1000*60*60*24)));
+        
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            } else {
+                hours.textContent = addZero(t.hours);
+                minutes.textContent = addZero(t.minutes);
+                seconds.textContent = addZero(t.seconds);
+            }
+        }
+        function addZero(num) {
+            if (num>=0 && num <10) {
+                return '0' + num;
+            } else {
+                return num;
+            }
+        }
+    }
+
+    setClock('timer', deadline);
 });
