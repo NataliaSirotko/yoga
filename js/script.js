a1a0101737d613d8a157265385f45a928aa551e6
malinaNata <kalinka007@tut.by>
2019-05-02 21:08:14 +0300

    add scroll

--- js/script.js
+++ js/script.js
@@ -84,4 +84,20 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 
     setClock('timer', deadline);
+
+    //Scroll
+    let anchors = document.querySelectorAll('a[href*="#"]');
+
+    for (let item of anchors) {
+        item.addEventListener('click', function(event) {
+            event.preventDefault();
+
+            let elemId = item.getAttribute('href');
+
+            document.querySelector('' + elemId).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    }
 });
