6938a669e5048185bdc145cacab18bbe2a7f7dad
malinaNata <kalinka007@tut.by>
2019-05-03 19:31:44 +0300

    add modal

--- js/script.js
+++ js/script.js
@@ -100,4 +100,32 @@ window.addEventListener('DOMContentLoaded', function() {
             });
         });
     }
+
+    //Modal
+
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close');
+
+    more.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    });
+
+    close.addEventListener('click', function() {
+        overlay.style.display = 'none';
+        more.classList.remove('.more-splash');
+        document.body.style.overflow = '';
+    });
+
+    let descriptionBtns = document.querySelectorAll('.description-btn');
+
+    descriptionBtns.forEach(item => {
+        item.addEventListener('click', function() {
+            overlay.style.display = 'block';
+            this.classList.add('more-splash');
+            document.body.style.overflow = 'hidden';
+        });
+    });
 });
