eda06343e96b941976d691d6bef98510c841e923
malinaNata <kalinka007@tut.by>
2019-05-17 21:47:39 +0300

    refactor code

--- js/script.js
+++ js/script.js
@@ -192,120 +192,119 @@ window.addEventListener('DOMContentLoaded', () => {
 
     getModal();
 
-    //Form
-    function sendForm() {
+    //Form   
+    function sendForm(form, popup) {
         let message = {
             loading: 'Загружается..',
             success: 'Спасибо, скоро мы с вами свяжемся',
             failure: 'Что-то пошло не так...'
         };
     
-        let form = document.getElementsByTagName('form'),
-            bottomForm = document.getElementsByTagName('form');
-    
-        for (let j=0; j< form.length; j++) {
-    
-            let input = form[j].getElementsByTagName('input'),
-                statusMessage = document.createElement('div');
+        let input = form.getElementsByTagName('input'),
+            statusMessage = document.createElement('div');
 
-            statusMessage.classList.add('status');
+        statusMessage.classList.add('status');
     
-            let inputTel = form[j].querySelector('[type="tel"]');
-    
-            inputTel.addEventListener('keyup', () => {
-                inputTel.value = inputTel.value.replace(/[^0-9\+]/g, '');
+        let inputTel = form.querySelector('[type="tel"]');
+
+        inputTel.addEventListener('keyup', () => {
+            inputTel.value = inputTel.value.replace(/[^0-9\+]/g, '');
+        });
+
+            
+        let img = document.createElement('img'); 
+
+        form.addEventListener('submit', (event) => {
+            event.preventDefault();
+            form.appendChild(statusMessage);
+            popup.appendChild(img);
+
+            let formData = new FormData(form);
+
+            let obj = {};
+            formData.forEach((value, key) => {
+                obj[key] = value;
             });
+            let json = JSON.stringify(obj);
 
-            let popup = document.querySelector('.popup-form'),
-                img = document.createElement('img'); 
+            function postData(data) {                  
 
-            form[j].addEventListener('submit', (event) => {
-                event.preventDefault();
-                form[j].appendChild(statusMessage);
-                popup.appendChild(img);
+                return new Promise(function(resolve, reject) {
+                    let request = new XMLHttpRequest();
 
-                let formData = new FormData(form[j]);
+                    request.open('POST', 'server.php');
 
-                let obj = {};
-                formData.forEach((value, key) => {
-                    obj[key] = value;
-                });
-                let json = JSON.stringify(obj);
-
-                function postData(data) {                  
-
-                    return new Promise(function(resolve, reject) {
-                        let request = new XMLHttpRequest();
-
-                        request.open('POST', 'server.php');
-
-                        //request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-                        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');  
+                    //request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+                    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');  
                         
-                        request.addEventListener('readystatechange', () => {
-                            if (request.readyState < 4) {
-                                resolve()                
-                            } else if (request.readyState === 4 && request.status == 200) {
-                                resolve()
-                            } else {
-                                reject()
-                            }      
-                        });
-
-                        request.send(data);
-                    })  
-               
-                }// end postData
-
-                function clearInput() {
-                    for (let i=0; i<input.length; i++) {
-                        input[i].value = '';
-                    }
-                }
-                
-                    postData(json)
-                        .then(() => {
-                            //statusMessage.innerHTML = message.loading;
-                            form[j].style.display = 'none';
-                            img.style.display = "block";
-                            img.src = "/icons/ajax-loader.gif";
-                            img.style.margin = "10px 240px 0";
-                        })
-                        .then(() => {
-                            //statusMessage.innerHTML = message.success;                      
-                            img.src = "/icons/herbal.png";
-                            img.style.width = "150px";
-                            console.log('отправлено');
-                        })
-                        .catch(() => {
-                            //statusMessage.innerHTML = message.failure;
-                            img.src = "/icons/fish.psd";
-                            img.style.width = "150px";
-                            console.log('ошибка');
-                        })
-                        .then(clearInput)
-
-                let more = document.querySelector('.more'),
-                    descrBtn = document.querySelectorAll('.description-btn');
-
-                more.addEventListener('click', () => {
-                    form[j].style.display = 'block';
-                    img.style.display = "none";
-                    //form[j].removeChild(statusMessage);
-                }); 
-                descrBtn.forEach(item => {
-                    item.addEventListener('click', () => {
-                        form[j].style.display = 'block';
-                        img.style.display = "none";
-                        //form[j].removeChild(statusMessage);
+                    request.addEventListener('readystatechange', () => {
+                        if (request.readyState < 4) {
+                            resolve()                
+                        } else if (request.readyState === 4 && request.status == 200) {
+                            resolve()
+                        } else {
+                            reject()
+                        }      
                     });
+
+                    request.send(data);
+                })  
+               
+            }// end postData
+
+            function clearInput() {
+                for (let i=0; i<input.length; i++) {
+                    input[i].value = '';
+                }
+            }
+                
+                postData(json)
+                    .then(() => {
+                        //statusMessage.innerHTML = message.loading;
+                        form.style.display = 'none';
+                        img.style.display = "block";
+                        img.src = "/icons/ajax-loader.gif";
+                        img.style.margin = "30px 200px 0";
+                    })
+                    .then(() => {
+                        //statusMessage.innerHTML = message.success;                      
+                        img.src = "/icons/herbal.png";
+                        img.style.width = "150px";
+                        console.log('отправлено');
+                    })
+                    .catch(() => {
+                        //statusMessage.innerHTML = message.failure;
+                        img.src = "/icons/fish.psd";
+                        img.style.width = "150px";
+                        console.log('ошибка');
+                    })
+                    .then(clearInput)
+
+            let more = document.querySelector('.more'),
+                descrBtn = document.querySelectorAll('.description-btn');
+
+            more.addEventListener('click', () => {
+                form.style.display = 'block';
+                img.style.display = "none";
+                //form.removeChild(statusMessage);
+            }); 
+            descrBtn.forEach(item => {
+                item.addEventListener('click', () => {
+                    form.style.display = 'block';
+                    img.style.display = "none";
+                    //form.removeChild(statusMessage);
                 });
             });
-                  
-        }   
+        });
+                    
     }
 
-    sendForm();  
+    let form = document.getElementsByTagName('form');
+    let popup = document.querySelectorAll('div[class$="-form"]');
+
+    sendForm(form[1], popup[1]);
+    sendForm(form[0], popup[0]);
+
 
     //Slider
 
